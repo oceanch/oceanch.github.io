00c18de00a18a414a87544aa2bfa53af75fac933
chong <manalgorithmuk@gmail.com>
2019-04-15 21:59:49 +0100

    php

--- mail.php
+++ mail.php
@@ -1,29 +1,16 @@
 <?php
-    $to = 'demo@spondonit.com';
-    $firstname = $_POST["fname"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
-    
-
-
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>Email: '.$text.'</td></tr>
-        
-    </table>';
-
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
-    }else{
-        echo 'failed';
-    }
-
-?>
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $call = $_POST['call'];
+    $website = $_POST['website'];
+    $priority = $_POST['priority'];
+    $type = $_POST['type'];
+    $message = $_POST['message'];
+    $formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
+    $recipient = "chong.han82@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
+    ?>
