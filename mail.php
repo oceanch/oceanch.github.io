addfac8ef6a33d083d712c15af1d0742d02f8116
chong <manalgorithmuk@gmail.com>
2019-04-15 22:06:42 +0100

    php

--- mail.php
+++ mail.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
     $name = $_POST['name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
@@ -15,33 +14,3 @@
     mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
     echo "Thank You!";
     ?>
-=======
-    $to = 'manalgorithmuk@gmail.com';
-    $firstname = $_POST["fname"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
-    
-
-
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>Email: '.$text.'</td></tr>
-        
-    </table>';
-
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
-    }else{
-        echo 'failed';
-    }
-
-?>
->>>>>>> master
