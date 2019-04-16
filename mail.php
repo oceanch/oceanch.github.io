b5aefb17d33ae3e5d10b6236a779cd512e8f51e1
chong <manalgorithmuk@gmail.com>
2019-04-16 20:46:16 +0100

    php

--- mail.php
+++ mail.php
@@ -8,9 +8,12 @@
     $type = $_POST['type'];
     $message = $_POST['message'];
     $formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
-    $recipient = "chong.han82@gmail.com";
+    $recipient = "chong.han@me.com";
     $subject = "Contact Form";
     $mailheader = "From: $email \r\n";
-    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    if (isset($_POST['send'])) {
+
+        mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
     echo "Thank You!";
+    }
     ?>
